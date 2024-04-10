03f85d9c0d5438dd91a0608b481302b45cfaec1a
YoussefAdel080 <113376103+YoussefAdel080@users.noreply.github.com>
2024-04-10 12:45:01 +0200

    ..
    ..

---
+++ Js/index.js
@@ -0,0 +1,29 @@
+const dateInput = document.getElementById('date-input');
+const calculateBtn = document.getElementById('calculate-btn');
+
+let currentDate = '';
+axios.get('http://worldtimeapi.org/api/timezone/Africa/Cairo').then(
+    res => 
+    {
+        currentDate = res.data.datetime.slice(0,10);
+    }
+).catch(err => {console.log(err)});
+
+const dateResults = Array.from(document.getElementsByClassName("date-result"));
+
+calculateBtn.addEventListener('click', (e) => 
+{   const enteredDateObj = new Date(dateInput.value);
+    const currentDateObj = new Date(currentDate);
+    let differenceInDays = Math.round((currentDateObj.getTime() - enteredDateObj.getTime())/(1000* 60 * 60 * 24))
+    let years = Math.floor(differenceInDays / 365);
+    differenceInDays -= (years*360);
+    let months = Math.floor(differenceInDays / 30);
+    differenceInDays -= (months * 30);
+    let days = Math.floor(differenceInDays);
+
+    for(let i = 0 ; i < dateResults.length ; i++){
+        dateResults[i].innerHTML = [years, months, days][i]
+    }
+}
+)
+
