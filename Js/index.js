e83b68aa76e9f178657fc9d9bd81e65fd85316de
YoussefAdel080 <113376103+YoussefAdel080@users.noreply.github.com>
2024-04-13 02:50:10 +0200

    some small changes
    modified some of the Js code

--- Js/index.js
+++ Js/index.js
@@ -2,12 +2,7 @@ const dateInput = document.getElementById('date-input');
 const calculateBtn = document.getElementById('calculate-btn');
 
 let currentDate = '';
-axios.get('http://worldtimeapi.org/api/timezone/Africa/Cairo').then(
-    res => 
-    {
-        currentDate = res.data.datetime.slice(0,10);
-    }
-).catch(err => {console.log(err)});
+getCurrentDate();
 
 const dateResults = Array.from(document.getElementsByClassName("date-result"));
 
@@ -16,7 +11,7 @@ calculateBtn.addEventListener('click', (e) =>
     const currentDateObj = new Date(currentDate);
     let differenceInDays = Math.round((currentDateObj.getTime() - enteredDateObj.getTime())/(1000* 60 * 60 * 24))
     let years = Math.floor(differenceInDays / 365);
-    differenceInDays -= (years*360);
+    differenceInDays -= (years*365);
     let months = Math.floor(differenceInDays / 30);
     differenceInDays -= (months * 30);
     let days = Math.floor(differenceInDays);
@@ -27,3 +22,12 @@ calculateBtn.addEventListener('click', (e) =>
 }
 )
 
+async function getCurrentDate(){
+    try {
+        const response = await axios.get('http://worldtimeapi.org/api/timezone/Africa/Cairo').then(
+            res => 
+            {
+                currentDate = res.data.datetime.slice(0,10);
+            }
+        ) }catch{(err => {console.log(err)})};
+};
